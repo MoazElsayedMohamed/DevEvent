96b9795139c7bf920dcc84f9993d21b4cce872e5
Moaz Elsayed <moazelsayed75@yahoo.com>
2026-02-15 21:46:15 +0200

    fix: remove cache enabling

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   typescript: { ignoreBuildErrors: true },
-  cacheComponents: true,
   images: {
     remotePatterns: [
       {
