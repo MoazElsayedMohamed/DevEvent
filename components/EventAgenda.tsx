8478a3c3ccca511583e9f6394284bd461a47baf4
Moaz Elsayed <moazelsayed75@yahoo.com>
2026-02-17 10:07:18 +0200

    fix: edit agenda title styling

--- components/EventAgenda.tsx
+++ components/EventAgenda.tsx
@@ -1,7 +1,7 @@
 const EventAgenda = ({ agendaItems }: { agendaItems: string[] }) => {
   return (
     <div className="flex flex-col gap-2">
-      <h2>Agenda</h2>
+      <h2 className="font-schibsted-grotesk text-2xl font-bold">Agenda</h2>
       <ul>
         {agendaItems.map((item) => (
           <li className="text-light-100 text-lg max-sm:text-sm" key={item}>
